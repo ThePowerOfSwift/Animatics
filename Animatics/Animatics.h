0cf1b7e8568fb456a9cdc041f6e818169faec7b1
Nikita Arkhipov <nikitarkhipov@gmail.com>
2016-11-02 18:43:46 +0300

    Initial Commit

---
+++ Animatics/Animatics.h
@@ -0,0 +1,19 @@
+//
+//  Animatics.h
+//  Animatics
+//
+//  Created by Nikita Arkhipov on 02.11.16.
+//  Copyright © 2016 Anvics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Animatics.
+FOUNDATION_EXPORT double AnimaticsVersionNumber;
+
+//! Project version string for Animatics.
+FOUNDATION_EXPORT const unsigned char AnimaticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Animatics/PublicHeader.h>
+
+
